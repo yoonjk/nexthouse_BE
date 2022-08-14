d0d4a1933fccb80e3f640851149191f7531bcf33
이동희 <eastshine96@naver.com>
2022-08-14 19:37:09 +0900

    jenkins 반영 테스트

--- src/main/java/com/demo/microservices/controller/RentMgntController.java
+++ src/main/java/com/demo/microservices/controller/RentMgntController.java
@@ -33,7 +33,7 @@ public class RentMgntController {
 	@Value("${garage.product.port}")
 	String productPort;
 	
-	@ApiOperation(value="전체 임대계약목록 정보 가져오기")
+	@ApiOperation(value="전체 임대계약목록 정보 가져오기####잘 변경되는지 테스트입니당")
 	@CrossOrigin
 	@GetMapping(value="/RentCntrList/{custNo}")
 	public ResponseEntity <List<RentCntr>> getRentCntrListAll(@PathVariable String custNo) { 
